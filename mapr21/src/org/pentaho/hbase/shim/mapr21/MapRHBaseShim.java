09c393a7b1fd1bb091ee0ff63b8cb62142740df1
Bryan Rosander <brosander@pentaho.com>
2013-11-05 10:10:56 -0500

    PDI-10558 - Making code change so that shim will work with hbase-0.94.5-mapr.jar from mapr

---
+++ mapr21/src/org/pentaho/hbase/shim/mapr21/MapRHBaseShim.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.hbase.shim.mapr21;
+
+import org.pentaho.hadoop.shim.ShimVersion;
+import org.pentaho.hbase.shim.spi.HBaseConnection;
+import org.pentaho.hbase.shim.spi.HBaseShim;
+
+/**
+ * Concrete implementation of HBaseShim suitable for use with Apache HBase 0.90.x.
+ * 
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
+ */
+public class MapRHBaseShim extends HBaseShim {
+
+  @Override
+  public ShimVersion getVersion() {
+    return new ShimVersion( 1, 0 );
+  }
+
+  public HBaseConnection getHBaseConnection() {
+    return new MapRHBaseConnection();
+  }
+}
