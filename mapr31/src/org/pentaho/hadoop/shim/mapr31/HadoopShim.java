41591b21cc44b27626ac79953994b741e57aca8f
Matt Burgess <mburgess@pentaho.com>
2014-03-04 14:22:23 -0500

    PDI-11306: Initial mapr31 shim

---
+++ mapr31/src/org/pentaho/hadoop/shim/mapr31/HadoopShim.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+*
+* Pentaho Big Data
+*
+* Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+*
+*******************************************************************************
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+******************************************************************************/
+
+package src.org.pentaho.hadoop.shim.mapr31;
+
+import java.util.List;
+
+import org.pentaho.hadoop.shim.HadoopConfiguration;
+import org.pentaho.hadoop.shim.HadoopConfigurationFileSystemManager;
+import org.pentaho.hadoop.shim.api.Configuration;
+import org.pentaho.hadoop.shim.common.CommonHadoopShim;
+import org.pentaho.hadoop.shim.common.DistributedCacheUtilImpl;
+import org.pentaho.hdfs.vfs.MapRFileProvider;
+
+public class HadoopShim extends CommonHadoopShim {
+
+  protected static final String DEFAULT_CLUSTER = "/";
+  protected static final String MFS_SCHEME = "maprfs://";
+  protected static final String[] EMPTY_CONNECTION_INFO = new String[2];
+
+  static {
+    JDBC_DRIVER_MAP.put("hive2",org.apache.hive.jdbc.HiveDriver.class); 
+  }
+
+  public HadoopShim() {
+    System.setProperty("mapr.library.flatclass", "");
+  }
+  
+  @Override
+  public String[] getNamenodeConnectionInfo(Configuration c) {
+    return EMPTY_CONNECTION_INFO;
+  }
+  
+  @Override
+  public String[] getJobtrackerConnectionInfo(Configuration c) {
+    return EMPTY_CONNECTION_INFO;
+  }
+
+  @Override
+  public void configureConnectionInformation(String namenodeHost, String namenodePort, String jobtrackerHost,
+      String jobtrackerPort, Configuration conf, List<String> logMessages) throws Exception {
+    if (namenodeHost == null || namenodeHost.length() == 0) {
+      namenodeHost = DEFAULT_CLUSTER;
+      logMessages.add("Using MapR default cluster for filesystem");
+    } else if (namenodePort == null || namenodePort.trim().length() == 0) {
+      logMessages.add("Using MapR CLDB named cluster: " + namenodeHost 
+          + " for filesystem");
+      namenodeHost = "/mapr/" + namenodeHost;
+    } else {
+      logMessages.add("Using filesystem at " + namenodeHost + ":" + namenodePort);
+      namenodeHost = namenodeHost + ":" + namenodePort;
+    }
+    
+    if (jobtrackerHost == null || jobtrackerHost.trim().length() == 0) {
+      jobtrackerHost = DEFAULT_CLUSTER;
+      logMessages.add("Using MapR default cluster for job tracker");
+    } else if (jobtrackerPort == null || jobtrackerPort.trim().length() == 0) {
+      logMessages.add("Using MapR CLDB named cluster: " + jobtrackerHost +
+      " for job tracker");
+      jobtrackerHost = "/mapr/" + jobtrackerHost;
+    } else {
+      logMessages.add("Using job tracker at " + jobtrackerHost + ":" + jobtrackerPort);
+      jobtrackerHost = jobtrackerHost + ":" + jobtrackerPort;
+    }
+    
+    String fsDefaultName = MFS_SCHEME + namenodeHost;
+    String jobTracker = MFS_SCHEME + jobtrackerHost;
+    conf.set("fs.default.name", fsDefaultName);
+    conf.set("mapred.job.tracker", jobTracker);
+    conf.set("fs.maprfs.impl", MapRFileProvider.FS_MAPR_IMPL);
+  }
+
+  @Override
+  public void onLoad(HadoopConfiguration config, HadoopConfigurationFileSystemManager fsm) throws Exception {
+    fsm.addProvider(config, MapRFileProvider.SCHEME, config.getIdentifier(), new MapRFileProvider());
+    setDistributedCacheUtil(new MapR3DistributedCacheUtilImpl(config));
+  }
+}
---
+++ mapr31/src/org/pentaho/hbase/shim/mapr31/MapRHBaseConnection.java
@@ -0,0 +1,128 @@
+package org.pentaho.hbase.shim.mapr31;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+import org.pentaho.hbase.shim.common.CommonHBaseConnection;
+
+public class MapRHBaseConnection extends CommonHBaseConnection {
+
+  protected <T> T doWithContextClassLoader( Callable<T> callable ) throws Exception {
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    Thread.currentThread().setContextClassLoader( getClass().getClassLoader() );
+    try {
+      return callable.call();
+    } finally {
+      Thread.currentThread().setContextClassLoader( cl );
+    }
+  }
+
+  @Override
+  public void createTable( final String tableName, final List<String> colFamilyNames, final Properties creationProps )
+    throws Exception {
+    doWithContextClassLoader( new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+
+        MapRHBaseConnection.super.createTable( tableName, colFamilyNames, creationProps );
+        return null;
+      }
+    } );
+  }
+
+  @Override
+  public void deleteTable( final String tableName ) throws Exception {
+    doWithContextClassLoader( new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+
+        MapRHBaseConnection.super.deleteTable( tableName );
+        return null;
+      }
+    } );
+  }
+
+  @Override
+  public void disableTable( final String tableName ) throws Exception {
+    doWithContextClassLoader( new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+
+        MapRHBaseConnection.super.disableTable( tableName );
+        return null;
+      }
+    } );
+  }
+
+  @Override
+  public void enableTable( final String tableName ) throws Exception {
+    doWithContextClassLoader( new Callable<Void>() {
+
+      @Override
+      public Void call() throws Exception {
+
+        MapRHBaseConnection.super.enableTable( tableName );
+        return null;
+      }
+    } );
+  }
+
+  @Override
+  public List<String> getTableFamiles( final String tableName ) throws Exception {
+    return doWithContextClassLoader( new Callable<List<String>>() {
+
+      @Override
+      public List<String> call() throws Exception {
+        return MapRHBaseConnection.super.getTableFamiles( tableName );
+      }
+    } );
+  }
+
+  @Override
+  public boolean isTableAvailable( final String tableName ) throws Exception {
+    return doWithContextClassLoader( new Callable<Boolean>() {
+
+      @Override
+      public Boolean call() throws Exception {
+        return MapRHBaseConnection.super.isTableAvailable( tableName );
+      }
+    } );
+  }
+
+  @Override
+  public boolean isTableDisabled( final String tableName ) throws Exception {
+    return doWithContextClassLoader( new Callable<Boolean>() {
+
+      @Override
+      public Boolean call() throws Exception {
+        return MapRHBaseConnection.super.isTableDisabled( tableName );
+      }
+    } );
+  }
+
+  @Override
+  public List<String> listTableNames() throws Exception {
+    return doWithContextClassLoader( new Callable<List<String>>() {
+
+      @Override
+      public List<String> call() throws Exception {
+        return MapRHBaseConnection.super.listTableNames();
+      }
+    } );
+  }
+
+  @Override
+  public boolean tableExists( final String tableName ) throws Exception {
+    return doWithContextClassLoader( new Callable<Boolean>() {
+
+      @Override
+      public Boolean call() throws Exception {
+        return MapRHBaseConnection.super.tableExists( tableName );
+      }
+    } );
+  }
+}
