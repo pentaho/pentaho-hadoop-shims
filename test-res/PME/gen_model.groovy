8285b8f38b142d5d68e5fbcdd263224c788f6091
Matt Burgess <mburgess@pentaho.com>
2014-03-18 15:57:57 -0400

    [TESTING]: Add PDI SuperJob and PME test helpers

--- test-res/PME/gen_model.groovy
+++ test-res/PME/gen_model.groovy
@@ -9,11 +9,11 @@ def file = new File(filename)
 def contents = file.text
 
 def outFile = new File(outFilename) 
-outFile.delete()
+outFile.delete()  
 outFile << contents \
     .replaceAll(~/\$\{Hive1Host\}/, p.get('Hive1Host','my.hostname.com')) \
     .replaceAll(~/\$\{Hive1Port\}/, p.get('Hive1Port','10000')) \
     .replaceAll(~/\$\{Hive2Host\}/, p.get('Hive2Host','my.hostname.com')) \
     .replaceAll(~/\$\{Hive2Port\}/, p.get('Hive2Port','10000')) \
-    .replaceAll(~/\$\{Hive2Host\}/, p.get('ImpalaHost','my.hostname.com')) \
-    .replaceAll(~/\$\{Hive2Port\}/, p.get('ImpalaPort','21050'))
+    .replaceAll(~/\$\{ImpalaHost\}/, p.get('ImpalaHost','my.hostname.com')) \
+    .replaceAll(~/\$\{ImpalaPort\}/, p.get('ImpalaPort','21050'))
