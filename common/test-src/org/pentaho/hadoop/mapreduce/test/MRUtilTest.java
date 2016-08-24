35e087444467da7bb14127e1595c493a47af1a20
Tatsiana Kasiankova <Tatsiana_Kasiankova@epam.com>
2016-08-24 18:16:38 +0300

    [BAD-540]: PMR mapper issue: Unexpected error registering jar plugin file kettle-engine-7.0-QAT-272.jar (#383)
    There are fix and unit tests.

--- common/test-src/org/pentaho/hadoop/mapreduce/test/MRUtilTest.java
+++ common/test-src/org/pentaho/hadoop/mapreduce/test/MRUtilTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Big Data
  *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -27,43 +27,45 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
 import org.junit.Test;
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.hadoop.mapreduce.MRUtil;
-
 
 /**
  * Tests for {@link MRUtil}.
  */
 public class MRUtilTest {
+  private static final String USER_DIR = System.getProperty( "user.dir" );
+  private static final String EXPECTED_DEFAULT_PLUGIN_DIR = USER_DIR + Const.FILE_SEPARATOR + "plugins";
+  private Configuration c;
+
+  @Before
+  public void setUp() {
+    c = new Configuration();
+  }
 
   @Test
   public void getPluginDirProperty() throws KettleException {
-    final String USER_DIR = System.getProperty( "user.dir" );
-
-    final Configuration c = new Configuration();
     assertNull( c.get( MRUtil.PROPERTY_PENTAHO_KETTLE_PLUGINS_DIR ) );
-
     String pluginDirProperty = MRUtil.getPluginDirProperty( c );
-    assertTrue( "Plugin Directory Property not configured as expected: " + pluginDirProperty,
-      pluginDirProperty.endsWith( USER_DIR ) );
+    assertTrue( "Plugin Directory Property not configured as expected: " + pluginDirProperty, pluginDirProperty
+        .endsWith( EXPECTED_DEFAULT_PLUGIN_DIR ) );
   }
 
   @Test
   public void getPluginDirProperty_explicitly_set() throws KettleException {
     final String PLUGIN_DIR = "/opt/pentaho";
-    final Configuration c = new Configuration();
     // Working directory will be used for the plugin directory if it is not explicitly provided
     c.set( MRUtil.PROPERTY_PENTAHO_KETTLE_PLUGINS_DIR, PLUGIN_DIR );
     String pluginDirProperty = MRUtil.getPluginDirProperty( c );
-    assertTrue( "Plugin Directory Property not configured as expected: " + pluginDirProperty,
-      pluginDirProperty.endsWith( PLUGIN_DIR ) );
+    assertTrue( "Plugin Directory Property not configured as expected: " + pluginDirProperty, pluginDirProperty
+        .endsWith( PLUGIN_DIR ) );
   }
 
   @Test
   public void getKettleHomeProperty() {
-    final String USER_DIR = System.getProperty( "user.dir" );
-    final Configuration c = new Configuration();
     String kettleHome = MRUtil.getKettleHomeProperty( c );
     assertEquals( USER_DIR, kettleHome );
   }
@@ -71,10 +73,10 @@ public class MRUtilTest {
   @Test
   public void getKettleHomeProperty_explicitly_set() {
     final String KETTLE_HOME = "/my/kettle";
-    final Configuration c = new Configuration();
     // Working directory will be used for Kettle Home if it is not explicitly provided
     c.set( MRUtil.PROPERTY_PENTAHO_KETTLE_HOME, KETTLE_HOME );
     String kettleHome = MRUtil.getKettleHomeProperty( c );
     assertEquals( KETTLE_HOME, kettleHome );
   }
+
 }
