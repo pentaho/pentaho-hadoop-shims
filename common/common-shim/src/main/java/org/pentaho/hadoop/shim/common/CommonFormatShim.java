2bd6d850fff15575bc890e0f64312aa96585e67e
Vasilina Terehova <vasilina_terehova@epam.com>
2017-07-25 19:52:31 +0300

    avro, orc, parquest inheritance

--- common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/CommonFormatShim.java
+++ common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/CommonFormatShim.java
@@ -2,26 +2,29 @@ package org.pentaho.hadoop.shim.common;
 
 
 import org.pentaho.hadoop.shim.ShimVersion;
-import org.pentaho.hadoop.shim.api.format.FormatSchema;
 import org.pentaho.hadoop.shim.api.format.InputFormat;
 import org.pentaho.hadoop.shim.api.format.OutputFormat;
+import org.pentaho.hadoop.shim.common.format.AvroInputFormat;
+import org.pentaho.hadoop.shim.common.format.OrcInputFormat;
+import org.pentaho.hadoop.shim.common.format.ParquetInputFormat;
 import org.pentaho.hadoop.shim.spi.FormatShim;
 
 public class CommonFormatShim implements FormatShim {
 
-  @Override public InputFormat inputFormat() {
-    return null;
+
+  @Override public InputFormat createInputFormat( FormatType type ) {
+    //here some factory that will create
+    if ( type == FormatType.AVRO ) {
+      return new AvroInputFormat();
+    } else if ( type == FormatType.ORC ) {
+      return new OrcInputFormat();
+    } else if ( type == FormatType.PARQUET ) {
+      return new ParquetInputFormat();
+    }
+    throw new IllegalArgumentException( "Not supported scheme format" );
   }
 
-  @Override public InputFormat inputFormat( FormatSchema formatSchema ) {
-    return null;
-  }
-
-  @Override public OutputFormat outputFormat() {
-    return null;
-  }
-
-  @Override public OutputFormat outputFormat( FormatSchema formatSchema ) {
+  @Override public OutputFormat createOutputFormat( FormatType type ) {
     return null;
   }
 
