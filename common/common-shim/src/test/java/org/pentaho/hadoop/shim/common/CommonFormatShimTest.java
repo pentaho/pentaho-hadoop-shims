59e3750b2ed0026d11cb96b9f93580d13a6758a0
Vasilina Terehova <vasilina_terehova@epam.com>
2017-08-03 16:13:40 +0300

    it test for hdfs read

--- common/common-shim/src/test/java/org/pentaho/hadoop/shim/common/CommonFormatShimTest.java
+++ common/common-shim/src/test/java/org/pentaho/hadoop/shim/common/CommonFormatShimTest.java
@@ -8,6 +8,7 @@ import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.hadoop.shim.api.format.RecordReader;
 import org.pentaho.hadoop.shim.common.format.PentahoParquetInputFormat;
 import org.pentaho.hadoop.shim.common.fs.FileSystemProxy;
+import org.pentaho.hdfs.vfs.HadoopFileSystemImpl;
 
 import java.io.IOException;
 
@@ -23,9 +24,33 @@ public class CommonFormatShimTest {
     PentahoParquetInputFormat pentahoParquetInputFormat = new PentahoParquetInputFormat( jobConfiguration,
       new FileSystemProxy( FileSystem.get( jobConfiguration ) ),
       "message PersonRecord {\n"
-        + "required string name;\n"
-        + "required string age;\n"
+        + "required binary name;\n"
+        + "required binary age;\n"
         + "}" );
+    RecordReader recordReader = pentahoParquetInputFormat.getRecordReader(pentahoParquetInputFormat.getSplits().get(0));
+    recordReader.forEach( rowMetaAndData -> {
+      RowMetaInterface rowMeta = rowMetaAndData.getRowMeta();
+      for (String fieldName : rowMeta.getFieldNames()) {
+        try {
+          System.out.println( fieldName + " " + rowMetaAndData.getString( fieldName, "" ) );
+        } catch ( KettleValueException e ) {
+          e.printStackTrace();
+        }
+      }
+    } );
+
+  }
+
+  @Test
+  public void testParquetReadSuccessHdfsFileSystem() throws IOException, InterruptedException {
+    ConfigurationProxy jobConfiguration = new ConfigurationProxy();
+    jobConfiguration.set( FileInputFormat.INPUT_DIR, "hdfs://svqxbdcn6cdh510n1.pentahoqa.com:8020/user/devuser/parquet" );
+    PentahoParquetInputFormat pentahoParquetInputFormat = new PentahoParquetInputFormat( jobConfiguration,
+            new FileSystemProxy( new HadoopFileSystemImpl(FileSystem.get( jobConfiguration )) ),
+            "message PersonRecord {\n"
+                    + "required binary name;\n"
+                    + "required binary age;\n"
+                    + "}" );
     RecordReader recordReader = pentahoParquetInputFormat.getRecordReader(pentahoParquetInputFormat.getSplits().get(0));
     recordReader.forEach( rowMetaAndData -> {
       RowMetaInterface rowMeta = rowMetaAndData.getRowMeta();
