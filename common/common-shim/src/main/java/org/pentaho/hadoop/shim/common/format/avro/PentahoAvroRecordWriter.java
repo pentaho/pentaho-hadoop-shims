b629794e4312ba4518b6805461a67dcabdf8dac1
Tim Kafalas tkafalas <tkafalas@pentaho.com>
2017-09-01 10:58:00 -0400

    [BACKLOG-18603] Avro implementation on Shim Layer

---
+++ common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/format/avro/PentahoAvroRecordWriter.java
@@ -0,0 +1,100 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.hadoop.shim.common.format.avro;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.pentaho.di.core.RowMetaAndData;
+import org.pentaho.di.core.exception.KettleValueException;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.hadoop.shim.api.format.IPentahoOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by tkafalas on 8/28/2017.
+ */
+public class PentahoAvroRecordWriter implements IPentahoOutputFormat.IPentahoRecordWriter {
+  private final DataFileWriter<GenericRecord> nativeAvroRecordWriter;
+  private final Schema schema;
+  private final TaskAttemptContext taskAttemptContext;
+
+  public PentahoAvroRecordWriter( DataFileWriter<GenericRecord> recordWriter, Schema schema,
+                                  TaskAttemptContext taskAttemptContext ) {
+    this.nativeAvroRecordWriter = recordWriter;
+    this.schema = schema;
+    this.taskAttemptContext = taskAttemptContext;
+  }
+
+  @Override
+  public void write( RowMetaAndData row ) {
+    RowMetaInterface rmi = row.getRowMeta();
+    GenericRecord outputRecord = new GenericData.Record( schema );
+
+    try {
+      //Build the avro row
+      for ( int i = 0; i < rmi.getValueMetaList().size(); i++ ) {
+        ValueMetaInterface vmi = rmi.getValueMeta( i );
+        switch ( vmi.getType() ) {
+          case ValueMetaInterface.TYPE_STRING:
+            outputRecord.put( vmi.getName(), row.getString( i, null ) );
+            break;
+          case ValueMetaInterface.TYPE_INTEGER:
+            outputRecord.put( vmi.getName(), row.getInteger( i ) );
+            break;
+          case ValueMetaInterface.TYPE_NUMBER:
+            outputRecord.put( vmi.getName(), row.getNumber( i, 0 ) );
+            break;
+          case ValueMetaInterface.TYPE_BIGNUMBER:
+            outputRecord.put( vmi.getName(), row.getBigNumber( i, null ) );
+            break;
+          case ValueMetaInterface.TYPE_DATE:
+            outputRecord.put( vmi.getName(), row.getInteger( i ) );
+            break;
+          case ValueMetaInterface.TYPE_BOOLEAN:
+            outputRecord.put( vmi.getName(), row.getBoolean( i, false ) );
+            break;
+          case ValueMetaInterface.TYPE_BINARY:
+            outputRecord.put( vmi.getName(), row.getBinary( i, null ) );
+            break;
+          default:
+            break;
+        }
+      }
+      //Now Append the row to the file
+      nativeAvroRecordWriter.append( outputRecord );
+    } catch ( IOException e ) {
+      throw new IllegalArgumentException( "some exception while writing avro", e );
+    } catch ( KettleValueException e ) {
+      throw new IllegalArgumentException( "some exception while writing avro", e );
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    nativeAvroRecordWriter.close();
+  }
+}
--- common/common-shim/src/test/java/org/pentaho/hadoop/shim/common/ConfigurationProxyTest.java
+++ common/common-shim/src/test/java/org/pentaho/hadoop/shim/common/ConfigurationProxyTest.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ *
+ * Pentaho Big Data
+ *
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.hadoop.shim.common;
 
 import org.apache.hadoop.fs.FileSystem;
@@ -18,16 +40,10 @@ import org.apache.hadoop.util.Progressable;
 import org.junit.Before;
 import org.junit.Test;
 import org.pentaho.hadoop.shim.common.fs.PathProxy;
-import org.pentaho.hbase.mapred.PentahoTableInputFormat;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Matchers.endsWith;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.withSettings;
 
 public class ConfigurationProxyTest {
 
@@ -94,7 +110,7 @@ public class ConfigurationProxyTest {
 
     PathProxy path1 = new PathProxy( "file://path1" );
     PathProxy path2 = new PathProxy( "file://path2" );
-    configurationProxy.setInputPaths( path1, path2);
+    configurationProxy.setInputPaths( path1, path2 );
 
     inputPaths = FileInputFormat.getInputPaths( configurationProxy );
     assertEquals( 2, inputPaths.length );
