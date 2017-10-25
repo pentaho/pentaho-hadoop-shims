6050ff5215c96bbca01f12868c0ca0e2593bc1da
Alexander Buloichik <alexander_buloichik@epam.com>
2017-10-25 17:15:59 +0300

    [BACKLOG-19493] Fix types processing for Parquet input/output (#620)

--- common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/format/parquet/PentahoParquetReadSupport.java
+++ common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/format/parquet/PentahoParquetReadSupport.java
@@ -23,23 +23,28 @@ package org.pentaho.hadoop.shim.common.format.parquet;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
-import org.apache.hadoop.conf.Configuration;
-import org.pentaho.di.core.RowMetaAndData;
-import org.pentaho.hadoop.shim.api.format.SchemaDescription;
-import org.pentaho.hadoop.shim.common.format.parquet.ParquetConverter.MyRecordMaterializer;
 //#if shim_type=="HDP" || shim_type=="EMR" || shim_type=="HDI"
+import org.apache.hadoop.conf.Configuration;
 import org.apache.parquet.hadoop.api.InitContext;
 import org.apache.parquet.hadoop.api.ReadSupport;
 import org.apache.parquet.io.api.RecordMaterializer;
 import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.Type;
 //#endif
 //#if shim_type=="CDH" || shim_type=="MAPR"
+//$import org.apache.hadoop.conf.Configuration;
 //$import parquet.hadoop.api.InitContext;
 //$import parquet.hadoop.api.ReadSupport;
 //$import parquet.io.api.RecordMaterializer;
 //$import parquet.schema.MessageType;
+//$import parquet.schema.Type;
 //#endif
+
+import org.pentaho.di.core.RowMetaAndData;
+import org.pentaho.hadoop.shim.api.format.SchemaDescription;
+import org.pentaho.hadoop.shim.common.format.parquet.ParquetConverter.MyRecordMaterializer;
 
 public class PentahoParquetReadSupport extends ReadSupport<RowMetaAndData> {
   ParquetConverter converter;
@@ -47,20 +52,39 @@ public class PentahoParquetReadSupport extends ReadSupport<RowMetaAndData> {
 
   @Override
   public ReadContext init( InitContext context ) {
-    String schemaStr = context.getConfiguration().get( "PentahoParquetSchema" );
-    if ( schemaStr == null || schemaStr.trim().isEmpty() ) {
+    String schemaStr = context.getConfiguration().get( ParquetConverter.PARQUET_SCHEMA_CONF_KEY );
+    if ( schemaStr == null ) {
       throw new RuntimeException( "Schema not defined in the PentahoParquetSchema key" );
     }
     schema = SchemaDescription.unmarshall( schemaStr );
+
     converter = new ParquetConverter( schema );
 
-    return new ReadContext( converter.createParquetSchema(), new HashMap<>() );
+    // get all fields from file's schema
+    MessageType fileSchema = context.getFileSchema();
+    Map<String, Type> fileFields = new TreeMap<>();
+    for ( Type f : fileSchema.getFields() ) {
+      fileFields.put( f.getName(), f );
+    }
+    fileSchema.getFields().clear();
+
+    // use only required fields
+    for ( SchemaDescription.Field f : schema ) {
+      Type ff = fileFields.get( f.formatFieldName );
+      if ( ff == null ) {
+        throw new RuntimeException( "Field " + f.formatFieldName + " not found in parquet file" );
+      } else {
+        fileSchema.getFields().add( ff );
+      }
+    }
+
+    return new ReadContext( fileSchema, new HashMap<>() );
   }
 
   @Override
   public RecordMaterializer<RowMetaAndData> prepareForRead( Configuration configuration,
                                                             Map<String, String> keyValueMetaData,
                                                             MessageType fileSchema, ReadContext readContext ) {
-    return new MyRecordMaterializer( converter, schema );
+    return new MyRecordMaterializer( converter );
   }
 }
