703a6ade9d70d197714cda6fca513ae7faa03ec4
Angel Ramos <angel.ramos@hitachivantara.com>
2018-09-13 14:11:45 -0400

    [BACKLOG-25512] Removed conditional compilation instructions

---
+++ common/common-format/src/main/java/org/pentaho/hadoop/shim/common/format/parquet/delegate/DelegateFormatFactory.java
@@ -0,0 +1,32 @@
+package org.pentaho.hadoop.shim.common.format.parquet.delegate;
+
+import org.pentaho.big.data.api.cluster.NamedCluster;
+import org.pentaho.hadoop.shim.common.format.parquet.delegate.apache.PentahoApacheInputFormat;
+import org.pentaho.hadoop.shim.common.format.parquet.delegate.apache.PentahoApacheOutputFormat;
+import org.pentaho.hadoop.shim.common.format.parquet.delegate.twitter.PentahoTwitterInputFormat;
+import org.pentaho.hadoop.shim.common.format.parquet.delegate.twitter.PentahoTwitterOutputFormat;
+
+public class DelegateFormatFactory {
+
+    public static Object getInputFormatInstance( NamedCluster namedCluster ) throws Exception {
+
+        String shimIdentifier = namedCluster.getShimIdentifier();
+        if (shimIdentifier.startsWith("cdh") || (shimIdentifier.startsWith("mapr") && !shimIdentifier.equals( "mapr60" ))) {
+            return new PentahoTwitterInputFormat( namedCluster );
+        } else {
+            return new PentahoApacheInputFormat( namedCluster );
+        }
+
+    }
+
+    public static Object getOutputFormatInstance( NamedCluster namedCluster ) throws Exception {
+
+        String shimIdentifier = namedCluster.getShimIdentifier();
+        if (shimIdentifier.startsWith("cdh") || (shimIdentifier.startsWith("mapr") && !shimIdentifier.equals( "mapr60" ))) {
+            return new PentahoTwitterOutputFormat( );
+        } else {
+            return new PentahoApacheOutputFormat( );
+        }
+
+    }
+}
