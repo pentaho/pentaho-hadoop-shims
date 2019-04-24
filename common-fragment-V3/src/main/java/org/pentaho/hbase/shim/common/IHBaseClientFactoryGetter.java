ddc822e513137b151c76753be6e16075be277e4a
Angel Ramos <angel.ramos@hitachivantara.com>
2019-04-24 10:02:39 -0400

    [BACKLOG-28655] Consolidated common fragments v2 and v3 with v1

--- common-fragment-V3/src/main/java/org/pentaho/hbase/shim/common/IHBaseClientFactoryGetter.java
+++
@@ -1,8 +0,0 @@
-package org.pentaho.hbase.shim.common;
-
-import org.apache.hadoop.conf.Configuration;
-import org.pentaho.hbase.factory.HBaseClientFactory;
-
-public interface IHBaseClientFactoryGetter {
-  HBaseClientFactory getHBaseClientFactory( Configuration configuration );
-}
