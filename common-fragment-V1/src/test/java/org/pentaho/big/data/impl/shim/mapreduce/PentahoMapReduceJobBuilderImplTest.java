3eeec13da5a81a66bff614127020e5fc3fc38fae
peterrinehart <peter.rinehart@hitachivantara.com>
2019-08-22 14:58:38 -0400

    [BACKLOG-28399] More cleanup

--- common-fragment-V1/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/PentahoMapReduceJobBuilderImplTest.java
+++ common-fragment-V1/src/test/java/org/pentaho/big/data/impl/shim/mapreduce/PentahoMapReduceJobBuilderImplTest.java
@@ -94,6 +94,7 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
