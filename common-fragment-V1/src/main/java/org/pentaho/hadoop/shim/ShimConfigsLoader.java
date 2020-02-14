915a9eff677fe3c2456c687007365641d2078807
Gary Kuntsevich <gary.kuntsevich@hitachivantara.com>
2020-02-14 16:00:23 -0500

    PDI-18600 reducing multiple warning for missing *-site.xml files

--- common-fragment-V1/src/main/java/org/pentaho/hadoop/shim/ShimConfigsLoader.java
+++ common-fragment-V1/src/main/java/org/pentaho/hadoop/shim/ShimConfigsLoader.java
@@ -41,10 +41,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -53,12 +55,15 @@ public class ShimConfigsLoader {
   private static final Class<?> PKG = ShimConfigsLoader.class; // for i18n purposes, needed by Translator2!!
   private static LogChannelInterface log = new LogChannel( ShimConfigsLoader.class.getName() );
 
+  public static Set<String> CLUSTER_NAME_FOR_LOGGING = new HashSet<String>();
+  public static Set<String> SITE_FILE_NAME = new HashSet<String>();
+
   public static final String CONFIGS_DIR_PREFIX =
-    "metastore" + File.separator + "pentaho" + File.separator + "NamedCluster" + File.separator + "Configs";
+          "metastore" + File.separator + "pentaho" + File.separator + "NamedCluster" + File.separator + "Configs";
 
   public static Properties loadConfigProperties( String additionalPath ) {
     return getConfigProperties(
-      getURLToResourceFile( ClusterConfigNames.CONFIGS_PROP.toString(), additionalPath ) );
+            getURLToResourceFile( ClusterConfigNames.CONFIGS_PROP.toString(), additionalPath ) );
   }
 
   // complexity rule suppressed because the level of nesting is not significant and moving logic to other methods
@@ -69,26 +74,26 @@ public class ShimConfigsLoader {
       FileObject currentPath = null;
       if ( additionalPath != null && !additionalPath.equals( "" ) ) {
         currentPath = KettleVFS.getFileObject(
-          Const.getKettleDirectory() + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath
-            + File.separator
-            + siteFileName );
+                Const.getKettleDirectory() + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath
+                        + File.separator
+                        + siteFileName );
 
         if ( currentPath.exists() ) {
           return currentPath.getURL();
         }
 
         currentPath = KettleVFS.getFileObject(
-          Const.getUserHomeDirectory() + File.separator + ".pentaho" + File.separator + CONFIGS_DIR_PREFIX
-            + File.separator + additionalPath + File.separator
-            + siteFileName );
+                Const.getUserHomeDirectory() + File.separator + ".pentaho" + File.separator + CONFIGS_DIR_PREFIX
+                        + File.separator + additionalPath + File.separator
+                        + siteFileName );
         if ( currentPath.exists() ) {
           return currentPath.getURL();
         }
 
         currentPath = KettleVFS.getFileObject(
-          Const.getUserHomeDirectory() + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath
-            + File.separator
-            + siteFileName );
+                Const.getUserHomeDirectory() + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath
+                        + File.separator
+                        + siteFileName );
         if ( currentPath.exists() ) {
           return currentPath.getURL();
         }
@@ -96,9 +101,9 @@ public class ShimConfigsLoader {
         // normal metastore locations failed, see if there's a metastore in the big-data-plugin folder
         // this should only exist if this instance of pentaho were created to run on a yarn cluster
         PluginInterface pluginInterface =
-          PluginRegistry.getInstance().findPluginWithId( LifecyclePluginType.class, "HadoopSpoonPlugin" );
+                PluginRegistry.getInstance().findPluginWithId( LifecyclePluginType.class, "HadoopSpoonPlugin" );
         currentPath = KettleVFS.getFileObject( pluginInterface.getPluginDirectory().getPath()
-          + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath + File.separator + siteFileName );
+                + File.separator + CONFIGS_DIR_PREFIX + File.separator + additionalPath + File.separator + siteFileName );
         if ( currentPath.exists() ) {
           return currentPath.getURL();
         }
@@ -110,18 +115,28 @@ public class ShimConfigsLoader {
         if ( shim.getId().equals( defaultShim ) ) {
           // only return the legacy folder if the shim still exists
           currentPath = KettleVFS.getFileObject(
-            LegacyShimLocator.getLegacyDefaultShimDir( defaultShim ).toString() + File.separator + siteFileName );
+                  LegacyShimLocator.getLegacyDefaultShimDir( defaultShim ) + File.separator + siteFileName );
           if ( currentPath.exists() ) {
             log.logBasic( BaseMessages.getString( PKG, "ShimConfigsLoader.UsingLegacyConfig" ) );
             return currentPath.getURL();
           }
         }
       }
-      log.logError( BaseMessages.getString( PKG, "ShimConfigsLoader.UnableToFindConfigs" ),
-        siteFileName, additionalPath );
+
+      // Work around to avoid multiple logging for VFS
+      if ( ( CLUSTER_NAME_FOR_LOGGING.isEmpty() ) || ( !CLUSTER_NAME_FOR_LOGGING.contains(additionalPath) ) ) {
+        SITE_FILE_NAME.clear();
+        log.logBasic( BaseMessages.getString( PKG, "ShimConfigsLoader.UnableToFindConfigs" ), siteFileName, additionalPath );
+        CLUSTER_NAME_FOR_LOGGING.add(additionalPath);
+        SITE_FILE_NAME.add(siteFileName);
+      } else if ( ( SITE_FILE_NAME.isEmpty() ) || ( !SITE_FILE_NAME.contains(siteFileName) ) ) {
+        log.logBasic( BaseMessages.getString( PKG, "ShimConfigsLoader.UnableToFindConfigs" ), siteFileName, additionalPath );
+        SITE_FILE_NAME.add(siteFileName);
+      }
+
     } catch ( KettleFileException | IOException ex ) {
       log.logError( BaseMessages.getString( PKG, "ShimConfigsLoader.ExceptionReadingFile" ),
-        siteFileName, additionalPath, ex.getStackTrace() );
+              siteFileName, additionalPath, ex.getStackTrace() );
     }
     return null;
   }
@@ -141,24 +156,24 @@ public class ShimConfigsLoader {
     for ( String propertyName : properties.stringPropertyNames() ) {
       if ( propertyName.startsWith( "java.system." ) ) {
         System.setProperty( propertyName.substring( "java.system.".length() ),
-          properties.get( propertyName ).toString() );
+                properties.get( propertyName ).toString() );
       }
     }
 
     addConfigsAsResources( additionalPath, configurationConsumer,
-      Arrays.stream( fileNames ).map( ClusterConfigNames::toString ).collect( Collectors.toList() ) );
+            Arrays.stream( fileNames ).map( ClusterConfigNames::toString ).collect( Collectors.toList() ) );
   }
 
   public static void addConfigsAsResources( String additionalPath, Consumer<? super URL> configurationConsumer,
                                             List<String> fileNames ) {
     fileNames.stream().map( siteFile -> getURLToResourceFile( siteFile, additionalPath ) )
-      .filter( Objects::nonNull ).forEach( configurationConsumer );
+            .filter( Objects::nonNull ).forEach( configurationConsumer );
   }
 
   private static String[] createSiteFilesArray() {
     return new String[] { ClusterConfigNames.CORE_SITE.toString(), ClusterConfigNames.HDFS_SITE.toString(),
-      ClusterConfigNames.YARN_SITE.toString(), ClusterConfigNames.MAPRED_SITE.toString(),
-      ClusterConfigNames.HBASE_SITE.toString(), ClusterConfigNames.HIVE_SITE.toString() };
+            ClusterConfigNames.YARN_SITE.toString(), ClusterConfigNames.MAPRED_SITE.toString(),
+            ClusterConfigNames.HBASE_SITE.toString(), ClusterConfigNames.HIVE_SITE.toString() };
   }
 
   private static Properties getConfigProperties( URL pathToConfigProperties ) {
